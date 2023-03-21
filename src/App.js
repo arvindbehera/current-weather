f6082f112566ed0e03e828adc27820c450870f90
Your Name <you@example.com>
2023-03-21 21:00:05 +0530

    temperature and search component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Weather from "./components/Weather";
 
 function App() {
   return (
     <div className="App">
-      <h1>Current Weather</h1>
+      <Weather />
     </div>
   );
 }
